a9edb27196b0da64e861a6769b4f8937184bc9de
x <x@192.168.0.156>
2015-11-22 00:49:04 +0100

    feat: Test Rojo Segunda Iteraccion
    Segunda iteraccion, test rojo, se prueba con JUnitParms los valores
    invalidos del constructor, Se espera IllegalArgumentException

--- src/test/java/com/mario/tdd/PhoenixTeamTest.java
+++ src/test/java/com/mario/tdd/PhoenixTeamTest.java
@@ -1,16 +1,25 @@
 package com.mario.tdd;
 
+import static junitparams.JUnitParamsRunner.$;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import com.mario.tdd.PhoenixTeam;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 /**
  * Test unitario de la clase {@link PhoenixTeam}
  */
+@RunWith(JUnitParamsRunner.class)
 public class PhoenixTeamTest {
 
 	private static final int THREE_BUGS_RESOLVED = 3;
+
+	public Object[] illegalNbOfBugs() {
+		return $(-10, -1);
+	}
 
 	/**
 	 * Test que prueba el constructor de la clase {@link PhoenixTeam}
@@ -22,5 +31,15 @@ public class PhoenixTeamTest {
 		Assert.assertEquals(THREE_BUGS_RESOLVED + " se han pasado al constructor pero solo "
 				+ phoenixTeam.getBugsResolved() + " fuerons devueltos", THREE_BUGS_RESOLVED,
 				phoenixTeam.getBugsResolved());
+	}
+
+	/**
+	 * Test que prueba valores invalidos del constructor
+	 * @param illegalNbOfBugs
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	@Parameters(method = "illegalNbOfBugs")
+	public void constructorShouldThrowExceptionForIllegalBugsNb(int illegalNbOfBugs) {
+		new PhoenixTeam(illegalNbOfBugs);
 	}
 }
