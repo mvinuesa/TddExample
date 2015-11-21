e12ba3dd1fcda8456ee64dd0362f16942a0bd3b7
x <x@192.168.0.156>
2015-11-22 00:15:14 +0100

    feat: Test en Verde
    Segunda fase del ciclo de vida del TDD.
    
    Una vez que teniamos un test en rojo, hacemos los cambios para que pase
    a verde.
    
    Se anade la logica necesaria para pasarlo, se crea una variables
    bugsResolved, y se rellena el constructor y el metodo getBugsResolved.

--- src/main/java/com/mario/tdd/PhoenixTeam.java
+++ src/main/java/com/mario/tdd/PhoenixTeam.java
@@ -1,14 +1,30 @@
 package com.mario.tdd;
 
+/**
+ * Clase que indica los bugs que ha resuelto el equipot Fenix
+ * 
+ * @author mvinuesa
+ *
+ */
 public class PhoenixTeam {
 
-	public PhoenixTeam(int i) {
-		// TODO Auto-generated constructor stub
+	int bugsResolved; 
+	
+	/**
+	 * Constructor principal
+	 * 
+	 * @param bugsResolved
+	 */
+	public PhoenixTeam(int bugsResolved) {
+		this.bugsResolved = bugsResolved;
 	}
 
-	public Object getBugResolved() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Devuelve del numero de bugs resueltos
+	 * @return
+	 */
+	public int getBugsResolved() {
+		return bugsResolved;
 	}
 
 }
--- src/test/java/com/mario/tdd/PhoenixTeamTest.java
+++ src/test/java/com/mario/tdd/PhoenixTeamTest.java
@@ -6,19 +6,19 @@ import org.junit.Test;
 import com.mario.tdd.PhoenixTeam;
 
 /**
- * Unit test for simple App.
+ * Test unitario de la clase {@link PhoenixTeam}
  */
 public class PhoenixTeamTest 
 {
-    /**
-     * Rigourous Test :-)
-     *
-     */
+
+	/**
+	 * Test que prueba el constructor de la clase {@link PhoenixTeam}
+	 */
 	@Test
     public void contructorMustSetBugResolve()
     {
         PhoenixTeam phoenixTeam = new PhoenixTeam(3);
         
-        Assert.assertEquals(3, phoenixTeam.getBugResolved());
+        Assert.assertEquals(3, phoenixTeam.getBugsResolved());
     }
 }
