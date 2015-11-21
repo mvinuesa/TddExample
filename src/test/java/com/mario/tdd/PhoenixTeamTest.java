79b6a122c143c48c0150f99076dbc6c96f089b7e
x <x@192.168.0.156>
2015-11-22 00:22:48 +0100

    feat: Refactorizacion del Primer ciclo de TDD
    Se refactoriza la clase o los test unitarios.
    
    El ciclo de vida de TDD termina con la refactorizacion del codigo,
    aunque sea solo anadir comentarios o refactorizacion de los test
    unitarios.

--- src/test/java/com/mario/tdd/PhoenixTeamTest.java
+++ src/test/java/com/mario/tdd/PhoenixTeamTest.java
@@ -8,17 +8,19 @@ import com.mario.tdd.PhoenixTeam;
 /**
  * Test unitario de la clase {@link PhoenixTeam}
  */
-public class PhoenixTeamTest 
-{
+public class PhoenixTeamTest {
+
+	private static final int THREE_BUGS_RESOLVED = 3;
 
 	/**
 	 * Test que prueba el constructor de la clase {@link PhoenixTeam}
 	 */
 	@Test
-    public void contructorMustSetBugResolve()
-    {
-        PhoenixTeam phoenixTeam = new PhoenixTeam(3);
-        
-        Assert.assertEquals(3, phoenixTeam.getBugsResolved());
-    }
+	public void contructorMustSetBugResolve() {
+		PhoenixTeam phoenixTeam = new PhoenixTeam(THREE_BUGS_RESOLVED);
+
+		Assert.assertEquals(THREE_BUGS_RESOLVED + " se han pasado al constructor pero solo "
+				+ phoenixTeam.getBugsResolved() + " fuerons devueltos", THREE_BUGS_RESOLVED,
+				phoenixTeam.getBugsResolved());
+	}
 }
